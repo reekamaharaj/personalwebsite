e472ad3cfb87e32afd6538d1ed7ede95d1c7f8cb
Joseph Walker <josephwalker@tred.com>
2020-08-11 22:08:09 -0500

    Add a router

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,26 @@
-<h1>Hello World</h1>
-<p>Hello from Tailwind.css</p>
+<script>
+    import { Router, Route, link } from "svelte-routing";
+
+    import A from "./pages/A.svelte";
+    import B from "./pages/B.svelte";
+</script>
+
+<Router>
+    <nav class="text-center p-8">
+        <a class="text-indigo-600" href="a" use:link>Page A</a>
+        |
+        <a class="text-indigo-600" href="b" use:link>Page B</a>
+    </nav>
+    <div class="flex items-center justify-center h-64 bg-gray-100">
+        <Route path="a">
+            <A />
+        </Route>
+        <Route path="b">
+            <B />
+        </Route>
+        <Route>
+            <!-- No path === Default Page (404? Homepage? Up to you.) -->
+            <p>Default Page</p>
+        </Route>
+    </div>
+</Router>
---
+++ src/pages/A.svelte
@@ -0,0 +1,1 @@
+<p>This is page A</p>
