a34dc3a51be2f2952207ccba48df5281081dc083
Joseph Walker <josephwalker@tred.com>
2020-08-11 21:45:52 -0500

    Setup tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    purge: ["./src/*.svelte"],
+    theme: {
+        fontFamily: {
+            display: ["Rubik", "sans-serif"],
+            body: ["Roboto", "sans-serif"],
+            sans: ["Roboto", "sans-serif"],
+        },
+    },
+};
