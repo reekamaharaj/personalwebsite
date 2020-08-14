4d9ead4b45efe5ee90589167d75fa13d7bdb004b
reekamaharaj <reekamaharaj@gmail.com>
2020-08-13 19:49:37 -0500

    minimal, very basic nav bar with mobile responsiveness

--- src/App.svelte
+++ src/App.svelte
@@ -4,17 +4,12 @@
     import Blog from "./pages/Blog.svelte";
     import Home from "./pages/Home.svelte";
     import About from "./pages/About.svelte";
+    import Navbar from "./components/Navbar.svelte";
 </script>
 
 <Router>
-    <nav class="text-center p-8">
-        <a class="text-indigo-600" href="portfolio" use:link>Portfolio</a>
-        |
-        <a class="text-indigo-600" href="blog" use:link>Blog</a>
-        |
-        <a class="text-indigo-600" href="about" use:link>About</a>
-    </nav>
-    <div class="flex items-center justify-center h-64 bg-gray-100">
+    <Navbar/>
+    <div class="flex items-center justify-center h-64">
         <Route path="portfolio">
             <Portfolio />
         </Route>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,55 @@
+<nav class="flex justify-between w-full bg-blue-500 text-white p-4">
+    <a href="/">
+        <span class="font-semibold text-xl tracking-tight">Relax. It's a Syntax thing</span>
+    </a>
+    <a class="block md:text-white mr-4" href="https://www.linkedin.com/in/reekamaharaj/" target="_blank" >LinkedIn</a>
+            <a class="block md:text-white mr-4" href="https://github.com/reekamaharaj" target="_blank" >Github</a>
+            <a class="block md:text-white mr-4" href="#" target="_blank" >Resume</a>
+    <div class="md:items-center md:w-auto flex">
+        <div class="md:flex hidden">
+            <a class="block md:text-white mr-4" href="/">Home</a>
+            <a class="block md:text-white mr-4" href="/portfolio">Portfolio</a>
+            <a class="block md:text-white mr-4" href="/blog">Blog</a>
+            <a class="block md:text-white mr-4" href="/about">About</a>
+
+        </div>
+    </div>
+</nav>
+
+<div class="bottomNav fixed bottom-0 w-full">
+    <nav
+        style="border:1px solid blue"
+        class="md:hidden bottom-0 w-full bg-gray-700 text-xs">
+        <ul
+            class="flex justify-around items-center text-white text-center
+            opacity-75 text-lg font-bold">
+            <li class="p-4 hover:bg-gray-500">
+                <a href="/">
+                    <span>Home</span>
+                </a>
+            </li>
+            |
+            <li class="p-4 hover:bg-gray-500">
+                <a href="/portfolio">
+                    <span>Portfolio</span>
+                </a>
+            </li>
+            |
+            <li class="p-4 hover:bg-gray-500">
+                <a href="/blog">
+                    <span>Blog</span>
+
+                </a>
+            </li>
+            |
+            <li class="p-4 hover:bg-gray-500">
+                <a href="/about">
+                    <span>About</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
+
+<!-- Reference from Tailwind Components. Bottom and Header Nav responsive.
+By sebageounity -->
