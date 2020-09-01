9d3758e42a0180c59d62efe5d88eff89fabd65fe
reekamaharaj <reekamaharaj@gmail.com>
2020-09-01 10:26:19 -0500

    updated navbar so resume is typed out. changed the look slightly

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,7 +1,7 @@
 <nav class="w-full py-4 bg-blue-800">
     <div class="w-full container mx-auto flex flex-wrap items-center justify-between">
 
-        <nav>
+        <div>
             <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
                 <li class="px-4 hover:text-gray-500 hover:underline">
                     <a href="/">
@@ -29,19 +29,31 @@
                 </li>
 
             </ul>
-        </nav>
-
-        <div class="flex items-center text-xl no-underline text-white pr-6">
-            <a class="pl-6" href="https://github.com/reekamaharaj" target="_blank">
-                <i class="fab fa-github hover:text-gray-500"></i>
-            </a>
-            <a class="pl-6" href="https://docs.google.com/document/d/1KY87YyA2zXU-ziE8twnXCYxniIWOZ4dZHHoLAcvMacQ/export?format=pdf" target="_blank">
-                <i class="far fa-file hover:text-gray-500"></i>
-            </a>
-            <a class="pl-6" href="https://www.linkedin.com/in/reekamaharaj/" target="_blank">
-                <i class="fab fa-linkedin hover:text-gray-500"></i>
-            </a>
         </div>
+
+        <div>
+            <ul class="flex items-center justify-between font-bold text-white uppercase no-underline">
+                <li class="px-4 hover:text-gray-500 hover:underline">
+                    <a class="text-xl" href="https://www.linkedin.com/in/reekamaharaj/" target="_blank">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </li>
+                |
+                <li class="px-4 hover:text-gray-500 hover:underline">
+                    <a class="text-xl" href="https://github.com/reekamaharaj" target="_blank">
+                        <i class="fab fa-github"></i>
+                    </a>
+                </li>
+                |
+                <li class="px-4 hover:text-gray-500 hover:underline text-sm">
+                    <a href="https://docs.google.com/document/d/1KY87YyA2zXU-ziE8twnXCYxniIWOZ4dZHHoLAcvMacQ/export?format=pdf" target="_blank">
+                        Resume
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
     </div>
 
 </nav>
