666132d557fb293addacbdc7fdfb8fe1e3b7ee95
reekamaharaj <reekamaharaj@gmail.com>
2020-09-03 13:03:26 -0500

    first blog post

--- src/pages/BlogPost.svelte
+++ src/pages/BlogPost.svelte
@@ -4,18 +4,175 @@
 
         <article class="flex flex-col shadow my-4 max-w-3xl">
             <!-- Article Image -->
-                <img src="https://images.unsplash.com/photo-1533073526757-2c8ca1df9f1c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="postImg">
-            <div class="bg-white flex flex-col justify-start p-6">
+            <img
+                src="https://images.unsplash.com/photo-1533073526757-2c8ca1df9f1c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                alt="postImg" />
+            <div class="bg-white flex flex-col justify-start p-16">
 
-                <h1 class="text-2xl font-bold pb-3">Why software development?</h1>
+                <h1 class="text-2xl font-bold pb-3">
+                    Why software development?
+                </h1>
                 <p class="text-sm pb-3 font-semibold">
                     By Reeka Maharaj, Published on September 2nd, 2020
                 </p>
-                <p class="pb-3">After getting a bachelor's in Civil Engineering and working in the field, why change careers to software development. --Need to write something here now heh.-- </p>
-        </article>
+                <p class="pb-3">
+                    After getting a bachelor's in Civil Engineering and working
+                    in the field, why change careers to software development.
+                </p>
+                <p class="pb-3">
+                    Seems like a small question, but it really is a huge
+                    question. The decision came from multiple directions. First,
+                    the concern on my career path. I love Civil Engineering and
+                    enjoyed the work, but looking at my future I wasn't sure if
+                    it fit the life I wanted.
+                </p>
+                <p class="pb-3">
+                    If you don't know about Civil
+                    Engineering, that's understandable. Here's what the Civil
+                    Engineering Wikipedia page has.
+                </p>
+                <p class="pb-3 px-6">
+                    Civil engineering is a professional engineering discipline that
+                    deals with the design, construction, and maintenance of the
+                    physical and naturally built environment, including public works
+                    such as roads, bridges, canals, dams, airports, sewerage
+                    systems, pipelines, structural components of buildings, and
+                    railways.
+                </p>
+                <p class="pb-3">
+                    If you sit and think about what is contained in that
+                    definition, you'll notice that it covers everything you
+                    encounter physically in life. Civil engineering is one of the
+                    oldest professions and as society grows, the scope of Civil
+                    engineering grows with it. I loved it because of how broad it
+                    is, how many subjects it stretches to cover. I love it because
+                    it is community service at its core. When people ask me what
+                    Civil engineers do, I usually answer with, "Well, if a civil
+                    engineer does their job perfectly, you won't know it. But if
+                    they don't do the best job, or their work degrades, you will
+                    notice right away."
+                </p>
+                <p class="pb-3">
+                    Basic examples. Utilities in your house. If
+                your water pressure drops, there's a civil engineer somewhere
+                helping to fix that. If you run into road construction. There's
+                a civil engineer somewhere working on that.
+                </p>
+                <p class="pb-3">
+                    Why did I pick it?
+                    Because that's exactly what I want my life to be. I want to help
+                    everyone I can, and I want to do it as quietly as possible. I
+                    want to be the nice ninja that fixes and supports everyone
+                    without them having to think about it. Why? Honestly I don't
+                    have a reason. I've spent days with my therapist really trying
+                    to understand my drive, we've just settled on me being a
+                    caretaker personality.
+                </p>
+                <p class="pb-3">
+                    So if it fit me so perfectly, why would I
+                leave?
+                </p>
+                <p class="pb-3">
+                    It is a profession that is integral to everyday life, and
+                as such, it is VERY demanding. City council meetings, working
+                with local government, working with commercial entities, working
+                with the public, working with other professionals. There are
+                many hats that one engineer has to wear. There are extra hours
+                outside of full time office hours that are needed. It's
+                grueling. It's thankless.
+                </p>
+                <p class="pb-3">
+                    As much as I love it. It took more
+                    from me than I got back. I love learning, I love understanding
+                    more than what I'm working on. Working full time I didn't have
+                    time to devote to learning about other topics. I didn't have the
+                    time or energy to learn on my own. The software I wanted to
+                    learn more about was expensive and I only had access to it on my
+                    work computer. The training classes were expensive and during
+                    work hours, often needed to be initiated by my employer. While
+                    my employers wanted to have me complete the training, they still
+                    needed me to be in the office.
+                </p>
+                <p class="pb-3">
+                    I had spent two years in the
+                    field and was starting to study for the Professional Engineering
+                    exam that I could take to get my license. It's a hard exam, and
+                    takes a lot of extra effort to prepare for. I realized this was
+                    the perfect time for me to really consider if this was going to
+                    be something I wanted to continue. If I did consider leaving,
+                    what would take its place?
+                </p>
+                <p class="pb-3">
+                    I love analyzing, understanding and
+                    solving problems. Most importantly, I love learning. I needed to
+                    find a career path that would require those skills but also
+                    provide me with vast resources so that I could learn on my own
+                    time. Something that would allow me to pursue further learning
+                    on my own. Software development was an obvious path for me. I
+                    have been around coding my whole life, my dad is a coder and I
+                    was surrounded by C++ and Oracle books for as long as I
+                    remember.
+                </p>
+                <p class="pb-3">
+                    I'll be honest. I avoided coding as long as I could.
+                Purely because of stubbornness. I don't like doing something
+                unless I know it was my idea. I had to come to it on my own, or
+                I was going to hate it.
+                </p>
+                <p class="pb-3">
+                    After leaving engineering I started
+                    working with nonprofits. My main focus was working with cat
+                    rescues and mental health advocacy nonprofits. While working
+                    with these organizations I had noticed a gap in technology. I
+                    noticed that things could be so much easier if the right
+                    application was created. But since they are nonprofits, there
+                    isn't financial support to create those applications. There
+                    isn't incentive for developers to create them since the
+                    organizations that would use them don't have funds to pay for
+                    them.
+                </p>
+                <p class="pb-3">
+                    I decided that I wanted to try and create things that
+                could help. But I needed to learn more about software
+                development before I could even think about that. I tried self
+                learning, and while that was going well it didn't last for long.
+                I didn't have the consistency I needed to keep the momentum
+                going. I always ended up getting overwhelmed by the amount of
+                information. I decided to sign up for a coding boot camp
+                finally. I had considered it before, but never felt comfortable
+                picking a program. But I got an email from the University of
+                Texas at Austin about their Full Stack coding boot camp and
+                signed up within a week. I have gotten my Bachelor's in Civil
+                engineering from UT Austin and trust them completely to have a
+                good program. All my doubts about if it was going to be worth it
+                were gone.
+                </p>
+                <p class="pb-3">
+                    I am so glad I signed up for it. I got the consistent
+                    push to keep learning. I didn't have the option to stop moving
+                    forward. I had people to talk to who were learning the same
+                    things and I had specific things to focus on building. I have
+                    the foundation I have been wanting and now I can learn on my
+                    own. I have a better understanding of what needs to be
+                    considered when creating a new project. I have the tools I need
+                    to make things work. Most importantly. I know what to google to
+                    help me make progress with whatever I am working on.
+                </p>
+                <p class="pb-3">
+                    So here I
+                    am. A junior software developer starting out in a new career
+                    path. I am excited to build everything I've wanted to build for
+                    years. I can keep learning and it isn't dependent on my
+                    employer. I don't even have to spend money on courses if I don't
+                    want to. As long as I have a computer and access to the
+                    internet, I can learn anything and start applying that new
+                    knowledge right away.
+                </p>
 
+                <p class="pb-3">So. That's why software development :D.</p>
+            </div>
+        </article>
 
     </section>
 
 </div>
-
